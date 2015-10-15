43ee58e77865328673daf19563ca093d4fa1e9e8
Vivek Gahunia <vgahunia@gmail.com>
2015-10-15 16:16:52 -0400

    Add fields to models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015154857) do
+ActiveRecord::Schema.define(version: 20151015183941) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20151015154857) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first"
+    t.string   "last"
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
