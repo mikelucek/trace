254bebe779856048fe30ca3d0c3f04bf0065185c
Mike Lucek <michael.s.lucek@gmail.com>
2015-10-15 20:07:49 -0400

    paperclip install and db migrate

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'autoprefixer-rails'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise'
+gem 'paperclip', '~> 4.1'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015183941) do
+ActiveRecord::Schema.define(version: 20151015232431) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -52,6 +52,10 @@ ActiveRecord::Schema.define(version: 20151015183941) do
     t.string   "photo"
     t.integer  "age"
     t.string   "school"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "kids", ["email"], name: "index_kids_on_email", unique: true
