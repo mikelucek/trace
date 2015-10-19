abf756bbfcc06ff3f8deaf9e2ab9fbd61b5e92ea
Mike Lucek <michael.s.lucek@gmail.com>
2015-10-19 17:15:48 -0400

    calculate averages and display

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -2,9 +2,42 @@ class KidsController < ApplicationController
 
 	def show
 		if current_admin
-			@kid = Kid.find(params[:kid_id])
+		@kid = Kid.find(params[:kid_id])
+		@scores = @kid.scores.all
+		@letters = []
+		@scores.each do |x|
+			if !@letters.include?(x.letter)
+				@letters << x.letter
+			end
 		end
+
+		puts "*************LETTERS SORT*******"
+		puts @letters.sort!
+
+		@hash = {}
+
+		@letters.each do |thisletter|
+			sum = 0
+			count = 0
+			@scores.each do |x|
+				if x.letter == thisletter
+					sum = sum + x.score
+					count = count + 1
+				end
+			end
+			average = sum/count
+			puts "**************THIS LETTER"
+			puts thisletter
+			puts "***************AVERAGE"
+			puts average
+			@hash[thisletter] = average
+
+		end
+
+		@hash
+
 	end
+end
 
 	private
   
